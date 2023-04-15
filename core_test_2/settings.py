55a933dc7f696ec8fc820756cdd483c3e7d4e1e6
Ivystu <ivystu0315@gmail.com>
2023-04-16 07:54:53 +0800

    test02

--- core_test_2/settings.py
+++ core_test_2/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATIC_URL = 'static/'
+STATIC_URL = 'test02/static/'
 
 STATICFILES_DIRS = os.path.join(STATIC_ROOT, 'static'),
 
